419c960233ddb04f793ddd8bdca8d143021c9ec3
Zahir-R <zahiriv4@gmail.com>
2026-04-05 00:10:39 -0400

    first commit

---
+++ Source/BulletRush/BulletRushGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BulletRushGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETRUSH_API ABulletRushGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
