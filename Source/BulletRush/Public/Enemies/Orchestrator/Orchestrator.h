64404049a7dc84b9b7d585cd473999ab5412c0c5
Angel-xyz <miguelmiranda14007@gmail.com>
2026-05-23 20:54:48 -0400

    New File: Se añande la clase Orchestrator, que será el jefe del videojuego

---
+++ Source/BulletRush/Public/Enemies/Orchestrator/Orchestrator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemies/BossBase.h"
+#include "Orchestrator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BULLETRUSH_API AOrchestrator : public ABossBase
+{
+	GENERATED_BODY()
+	
+};
