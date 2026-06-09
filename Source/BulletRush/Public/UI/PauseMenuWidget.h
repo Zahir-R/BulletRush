70ec849a1208347bc2ac5e07c93b999eaec13f0a
soritsu <kevinguerra123.com@gmail.com>
2026-06-09 12:17:20 -0400

    menus mejorados

--- Source/BulletRush/Public/UI/PauseMenuWidget.h
+++ Source/BulletRush/Public/UI/PauseMenuWidget.h
@@ -5,6 +5,10 @@
 #include "PauseMenuWidget.generated.h"
 
 class UButton;
+class UImage;
+class USizeBox;
+class UTextBlock;
+class UCanvasPanel;
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnPauseMenuEvent);
 
@@ -22,16 +26,31 @@ public:
 
 protected:
     virtual void NativeOnInitialized() override;
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
 
 private:
     void CreateWidgetTree();
-    UButton* CreateMenuButton(const FString& Text, bool bIsVolverAlMapa);
+    UButton* CreateMenuButton(const FString& Text, bool bIsVolverAlMapa, USizeBox*& OutSizeBox, UTextBlock*& OutLabel);
+
     UFUNCTION() void OnVolverClicked();
     UFUNCTION() void OnContinuarClicked();
 
-    UPROPERTY()
-    UTexture2D* FondoTex;
+    // Referencias internas para el caos visual en el Tick
+    UPROPERTY() UImage* OverlayBgRef;
+    UPROPERTY() UButton* VolverBtnRef;
+    UPROPERTY() USizeBox* VolverSizeBoxRef;
+    UPROPERTY() UTextBlock* VolverLabelRef;
 
-    UPROPERTY()
-    UTexture2D* BotonTex;
+    UPROPERTY() UButton* ContinuarBtnRef;
+    UPROPERTY() USizeBox* ContinuarSizeBoxRef;
+    UPROPERTY() UTextBlock* ContinuarLabelRef;
+
+    // Controladores de interpolación fluida
+    float VolverScaleTarget;
+    float VolverCurrentScale;
+    float ContinuarScaleTarget;
+    float ContinuarCurrentScale;
+
+    UPROPERTY() UTexture2D* FondoTex;
+    UPROPERTY() UTexture2D* BotonTex;
 };
